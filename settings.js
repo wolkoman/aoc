2824d7a1ac1f9e8c8b1dd4695be8c72d38d70dcb
Manuel Wolkowitsch <superwolko@gmail.com>
2019-12-04 12:22:52 +0100

    Make resettable

---
+++ settings.js
@@ -0,0 +1,18 @@
+// URL of aventofcode host
+const http = (year,day) => `https://adventofcode.com/${year}/day/${day}/input`;
+
+// relative path to cookie file
+// MUST BE OF FOLLOWING FORM
+// session={{YOUR SESSION COOKIE}};
+const cookiePath = `cookie.txt`;
+
+// file with current year (e.g. 2019)
+const yearPath = `year.txt`;
+
+// path to the individual payload file
+const payloadPath = (day) => `day${lz(day)}/payload.txt`;
+
+// helper
+const lz = (x) => (x<10?'0':'')+x;
+
+module.exports = {lz, http, cookiePath, yearPath, payloadPath};
